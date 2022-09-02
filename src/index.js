7dc67235cd06ac166feb8e116014fedb9bf6d424
Oleksii Shevchenko <vazerius@gmail.com>
2022-09-02 11:57:09 +0300

    +

--- src/index.js
+++ src/index.js
@@ -23,9 +23,17 @@ async function markupGallery(data) {
   console.log(data);
   const markup = await data
     .map(el => {
+      let title = el.original_title;
+      if (title === undefined) {
+        title = el.original_name;
+        return `   <li class="gallery_item"><a href="">
+        <img class="gallery_img" src="https://image.tmdb.org/t/p/w500${el.poster_path}" alt="${el.title}"/></a>
+          <div><h2 class="gallery_title">${title}<span>ganre</span></h2></div>
+      </li>`;
+      }
       return `   <li class="gallery_item"><a href="">
-        <img class="gallery_img" src="https://image.tmdb.org/t/p/w500${el.poster_path}" alt="${el.original_title}"/></a>
-          <div><h2 class="gallery_title">${el.name}<span>ganre</span></h2></div>
+        <img class="gallery_img" src="https://image.tmdb.org/t/p/w500${el.poster_path}" alt="${el.title}"/></a>
+          <div><h2 class="gallery_title">${el.title}<span>ganre</span></h2></div>
       </li>`;
     })
     .join('');
